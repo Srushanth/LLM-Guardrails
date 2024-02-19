4d81487a3a6cab8e14911ded8a8e34c9aed1d8f5
Ubuntu <ubuntu@ip-172-31-46-189.eu-north-1.compute.internal>
2024-02-19 09:30:47 +0000

    Formatted the code.

--- src/download_model.py
+++ src/download_model.py
@@ -1,12 +1,17 @@
+"""A module that downloads a model from HuggingFace Hub and prints its size."""
+
 # Import the argparse module
 import argparse
 import requests
 from huggingface_hub import snapshot_download
+
 # Import the tqdm module
 from tqdm import tqdm
 
 # Create the parser
-parser = argparse.ArgumentParser(description="A simple script that takes two input arguments and prints them")
+parser = argparse.ArgumentParser(
+    description="A simple script that takes two input arguments and prints them"
+)
 
 # Add the arguments
 parser.add_argument("--repo_id", type=str, help="HuggingFace repo id", required=True)
@@ -16,11 +21,20 @@ parser.add_argument("--revision", type=str, help="Model revision", default="main
 args = parser.parse_args()
 
 # Print the arguments only when the script is run directly
-if __name__=="__main__":
+if __name__ == "__main__":
     # Get the number of files to be downloaded
-    response = requests.head(f"https://huggingface.co/{args.repo_id}/resolve/{args.revision}/")
+    response = requests.head(
+        f"https://huggingface.co/{args.repo_id}/resolve/{args.revision}/", timeout=10
+    )
     file_count = int(response.headers.get("Content-Length", 0))
-    
+
     # Wrap the snapshot_download function with tqdm function
-    for _ in tqdm(snapshot_download(repo_id=args.repo_id, revision=args.revision, local_dir=f"../models/{args.repo_id}/{args.revision}"), total=file_count):
+    for _ in tqdm(
+        snapshot_download(
+            repo_id=args.repo_id,
+            revision=args.revision,
+            local_dir=f"../models/{args.repo_id}/{args.revision}",
+        ),
+        total=file_count,
+    ):
         pass
